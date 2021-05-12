a5c18127110342145c567665c402e8622c5b5a87
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2021-05-12 10:07:54 +0700

    Created first 3 basic cases

---
+++ test/case3/Main.java
@@ -0,0 +1,18 @@
+package case3;
+import java.util.Scanner;
+
+public class Main {
+    static double manhattanDistance(double ax, double ay, double bx, double by) {
+        return Math.abs(ax - bx) + Math.abs(ay - by);
+    }
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        var ax = scanner.nextDouble();
+        var ay = scanner.nextDouble();
+        var bx = scanner.nextDouble();
+        var by = scanner.nextDouble();
+        var res = manhattanDistance(ax, ay, bx, by);
+        System.out.printf("Manhattan Distance: %.2f", res);
+    }
+}
