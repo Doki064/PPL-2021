f245e8132b69155423367d5bd6ea56c0e76d505a
Doki064 <52833936+Doki064@users.noreply.github.com>
2021-05-20 10:16:22 +0700

    Error when simplifying :v

--- src/symbol_table/__init__.py
+++ src/symbol_table/__init__.py
@@ -57,30 +57,30 @@ class SymbolTable(OrderedDict):
         self.current_token = self.next_token
         self.next_token = next(self.tokens, None)
 
-    def _check_token(self, *args):
+    def _check_token(self, token, *args):
         if len(args) == 1:
             if isinstance(args[0], str) or isinstance(args[0], token_names.Enum):
-                return self.current_token.token_name == args[0]
+                return token.token_name == args[0]
             elif isinstance(args[0], Iterable):
-                return self.current_token.token_name in args[0]
+                return token.token_name in args[0]
         raise TypeError("_check_token() taking 1 argument, type: str, Enum or Iterable object")
 
     def _generate(self):
         """Function for generating a symbol table."""
         scope_level = -1
-        identifier_type = None
+        identifier_type = []
         attributes = []
 
         while self.current_token is not None:
-            if self._check_token(token_names.IDENTIFIER):
+            if self._check_token(self.current_token, token_names.IDENTIFIER):
                 identifier_key = identifier_position = self.current_token.position
                 identifier_name = self.current_token.value
-                if self._check_token(token_names.Separators("[")):
+                if self._check_token(self.next_token, token_names.Separators("[")):
                     while True:
                         self._advance()
                         identifier_name += self.current_token.value
-                        if (self._check_token(token_names.Separators("]"))
-                                and not self._check_token(token_names.Separators("["))):
+                        if (self._check_token(self.current_token, token_names.Separators("]"))
+                                and not self._check_token(self.next_token, token_names.Separators("["))):
                             break
                 if scope_level == -1:
                     scope = "outer_scope"
@@ -115,32 +115,32 @@ class SymbolTable(OrderedDict):
                     "attributes": tuple(attributes),
                     "scope": (scope, scope_level),
                 }
-                identifier_type = None
+                identifier_type.clear()
                 attributes.clear()
 
-            elif self._check_token(token_names.KeywordsType.names()):
-                if self._check_token(token_names.Separators("[")):
+            elif self._check_token(self.current_token, token_names.KeywordsType.names()):
+                if self._check_token(self.next_token, token_names.Separators("[")):
                     identifier_type = self.current_token.value
                     while True:
                         self._advance()
                         identifier_type += self.current_token.value
-                        if (self._check_token(token_names.Separators("]"))
-                                and not self._check_token(token_names.Separators("["))):
+                        if (self._check_token(self.current_token, token_names.Separators("]"))
+                                and not self._check_token(self.next_token, token_names.Separators("["))):
                             break
-                elif self._check_token(token_names.IDENTIFIER):
+                elif self._check_token(self.next_token, token_names.IDENTIFIER):
                     identifier_type = self.current_token.value
 
-            elif self._check_token(token_names.KeywordsAttribute.names()):
-                if (self._check_token(token_names.KeywordsAttribute.names())
-                        or self._check_token(token_names.KeywordsType.names())):
+            elif self._check_token(self.current_token, token_names.KeywordsAttribute.names()):
+                if (self._check_token(self.next_token, token_names.KeywordsAttribute.names())
+                        or self._check_token(self.next_token, token_names.KeywordsType.names())):
                     attributes.append(self.current_token.value)
 
-            elif (self._check_token(token_names.Separators("{"))
-                  or self._check_token(token_names.Separators("("))):
+            elif (self._check_token(self.current_token, token_names.Separators("{"))
+                  or self._check_token(self.current_token, token_names.Separators("("))):
                 scope_level += 1
 
-            elif (self._check_token(token_names.Separators("}"))
-                  or self._check_token(token_names.Separators(")"))):
+            elif (self._check_token(self.current_token, token_names.Separators("}"))
+                  or self._check_token(self.current_token, token_names.Separators(")"))):
                 scope_level -= 1
 
             self._advance()
