cd8c2eae51ac0ac1da96bff47a56861816225327
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2021-05-21 22:49:04 +0700

    added basic parse tree visualization

--- src/ast/__init__.py
+++ src/ast/__init__.py
@@ -1,396 +1,423 @@
 __all__ = [
-    "programTree",
-    "blockTree",
-    "declrTree",
-    "funcDeclTree",
-    "funcHeadTree",
-    "typeTree",
-    "idTree",
-    "numberTree",
-    "stringTree",
-    "assignTree",
-    "ifTree",
-    "whileTree",
-    "returnTree",
-    "callTree",
-    "relOPTree",
-    "addOPTree",
-    "multOPTree",
+	"programTree",
+	"blockTree",
+	"declrTree",
+	"funcDeclTree",
+	"funcHeadTree",
+	"typeTree",
+	"idTree",
+	"numberTree",
+	"stringTree",
+	"assignTree",
+	"ifTree",
+	"whileTree",
+	"returnTree",
+	"callTree",
+	"relOPTree",
+	"addOPTree",
+	"multOPTree",
 ]
 
 from abc import ABC as _ABC
 
 
 class _AST(_ABC):
-    """ A simple Abstract Syntax Tree structure.
+	""" A simple Abstract Syntax Tree structure.
 
-    Contains the root label and children ASTs.
+	Contains the root label and children ASTs.
 
-    Attributes:
-        nodeCount (static int): How many nodes in the program tree.
-        _kids (list): List of immediate children AST.
-        _label (str): Label of the root node.
-        _nodeNum (int): The index of the root node in the program tree.
-    """
+	Attributes:
+		nodeCount (static int): How many nodes in the program tree.
+		_kids (list): List of immediate children AST.
+		_label (str): Label of the root node.
+		_nodeNum (int): The index of the root node in the program tree.
+	"""
 
-    nodeCount = 0
+	nodeCount = 0
 
-    def __init__(self, label):
-        """AST constructor.
+	def __init__(self, label):
+		"""AST constructor.
 
-        Args:
-            label (int): The root node's label.
-        """
+		Args:
+			label (int): The root node's label.
+		"""
 
-        super().__init__()
-        self._kids = []
-        _AST.nodeCount += 1
-        self._nodeNum = _AST.nodeCount
-        self._label = label
+		super().__init__()
+		self._kids = []
+		_AST.nodeCount += 1
+		self._nodeNum = _AST.nodeCount
+		self._label = label
 
-    def getKid(self, idx):
-        """Return the child AST at the given index.
+	def getKid(self, idx):
+		"""Return the child AST at the given index.
 
-        Args:
-            idx (int): The index of the child AST.
+		Args:
+			idx (int): The index of the child AST.
 
-        Returns:
-            (AST) The child AST if exists, None if index out of bounds.
-        """
-        if idx <= 0 or idx > self.kidCount():
-            return None
-        return self._kids[idx - 1]
+		Returns:
+			(AST) The child AST if exists, None if index out of bounds.
+		"""
+		if idx <= 0 or idx > self.kidCount():
+			return None
+		return self._kids[idx - 1]
 
-    def getKids(self):
-        """Return a list containing the children ASTs of the root node.
-        
-        Returns:
-            (List) List of children ASTs.
-        """
-        return self._kids
+	def getKids(self):
+		"""Return a list containing the children ASTs of the root node.
 
-    def kidCount(self):
-        """Return the number of children ASTs of the root node.
+		Returns:
+			(List) List of children ASTs.
+		"""
+		return self._kids
 
-        Returns:
-            (int) The number of children ASTs.
-        """
-        return len(self._kids)
+	def kidCount(self):
+		"""Return the number of children ASTs of the root node.
 
-    def addKid(self, kidAST):
-        """Add an AST to the children list of the root node.
+		Returns:
+			(int) The number of children ASTs.
+		"""
+		return len(self._kids)
 
-        Args:
-            kidAST (AST): The child AST to be added
+	def addKid(self, kidAST):
+		"""Add an AST to the children list of the root node.
 
-        Returns:
-            (AST) The AST of the root node.
-        """
-        self._kids.append(kidAST)
-        return self
+		Args:
+			kidAST (AST): The child AST to be added
 
-    def setLabel(self, label):
-        """Set the label of the root node.
+		Returns:
+			(AST) The AST of the root node.
+		"""
+		self._kids.append(kidAST)
+		return self
 
-        Args:
-            label (str): The label to be set.
+	def setLabel(self, label):
+		"""Set the label of the root node.
 
-        Returns:
-            None
-        """
-        self._label = label
+		Args:
+			label (str): The label to be set.
 
-    def getLabel(self):
-        """Return the label of the AST's root node.
+		Returns:
+			None
+		"""
+		self._label = label
 
-        Returns:
-            (str) The label of the AST's root node.
-        """
-        return self._label
+	def getLabel(self):
+		"""Return the label of the AST's root node.
+
+		Returns:
+			(str) The label of the AST's root node.
+		"""
+		return self._label
+
+	def getNodeNum(self):
+		"""Return the nosw number of the AST's root node.
+
+		Returns:
+			(int) The number of the AST's root node.
+		"""
+		return self._nodeNum
+
 
 #                      GRAMMAR NOTES                                  #
 # --------------------------------------------------------------------#
 #               Children trees are noted by '*'                       #
 #                   [] stands for a list of                           #
 class programTree(_AST):
-    """ An AST for the program structure.
+	""" An AST for the program structure.
 
-        GRAMMAR:
-            program    :-   class *id *block
-    """
-    def __init__(self):
-        super().__init__('Program/Class')
+		GRAMMAR:
+			program    :-   class *id *block
+	"""
+
+	def __init__(self):
+		super().__init__('Program/Class')
 
 
 class blockTree(_AST):
-    """ An AST for a codeblock structure.
+	""" An AST for a codeblock structure.
 
-        GRAMMAR:
-            block   :-  { [*statements] }
-    """
-    def __init__(self):
-        super().__init__('Code block')
+		GRAMMAR:
+			block   :-  { [*statements] }
+	"""
+
+	def __init__(self):
+		super().__init__('Code block')
 
 
 class declrTree(_AST):
-    """ An AST for a declaration statement structure.
+	""" An AST for a declaration statement structure.
 
-        GRAMMAR:
-            declr   :-  *type *id = *expr ; 
-                        *type *id ;
-                        *type *id 
-    """
-    def __init__(self):
-        super().__init__('Declaration')
+		GRAMMAR:
+			declr   :-  *type *id = *expr ;
+						*type *id ;
+						*type *id
+	"""
+
+	def __init__(self):
+		super().__init__('Declaration')
 
 
 class funcDeclTree(_AST):
-    """ An AST for a function declaration structure.
+	""" An AST for a function declaration structure.
 
-        GRAMMAR:
-            funcDeclr   :-  *type *id *funcHead *block
-    """
-    def __init__(self):
-        super().__init__('Function Declaration')
+		GRAMMAR:
+			funcDeclr   :-  *type *id *funcHead *block
+	"""
+
+	def __init__(self):
+		super().__init__('Function Declaration')
 
 
 class funcHeadTree(_AST):
-    """ An AST for a function header structure.
+	""" An AST for a function header structure.
 
-        GRAMMAR:
-            funcHead   :-   ()               #   void
-                            ( [*declr] )     #   params / list of declr
-    """
-    def __init__(self):
-        super().__init__('Function header')
+		GRAMMAR:
+			funcHead   :-   ()               #   void
+							( [*declr] )     #   params / list of declr
+	"""
+
+	def __init__(self):
+		super().__init__('Function header')
+
 
 class typeTree(_AST):
-    """ An AST for a type structure.
-        ATOMIC/LEAF
+	""" An AST for a type structure.
+		ATOMIC/LEAF
 
-        GRAMMAR:
-            type   :-   type
+		GRAMMAR:
+			type   :-   type
 
-        Args:
-            isList (bool): whether the type is for a single or a list/array of variables.
-                            E.g: String vs String[]
-    """
-    def __init__(self, isList=False):
-        super().__init__('Type')
-        self.isArray = isList
+		Args:
+			isList (bool): whether the type is for a single or a list/array of variables.
+							E.g: String vs String[]
+	"""
 
-    def setArray(self):
-        """ Set the value of isList to True
+	def __init__(self, isList=False):
+		super().__init__('Type')
+		self.isArray = isList
 
-        Returns:
-            None
-        """
-        self.isArray = True
+	def setArray(self):
+		""" Set the value of isList to True
 
-    def isArray(self):
-        """ Return the value of isList.
+		Returns:
+			None
+		"""
+		self.isArray = True
 
-        Returns:
-            (bool) whether the type is an array or not.
-        """
-        return self.isArray
+	def isArray(self):
+		""" Return the value of isList.
+
+		Returns:
+			(bool) whether the type is an array or not.
+		"""
+		return self.isArray
 
 
 class idTree(_AST):
-    """ An AST for a identifier or name.
-        ATOMIC/LEAF
+	""" An AST for a identifier or name.
+		ATOMIC/LEAF
 
-        GRAMMAR:
-            id   :-   id/name
+		GRAMMAR:
+			id   :-   id/name
 
-        Args:
-            name (str): name of the identifier.
-    """
-    def __init__(self, name):
-        super().__init__('id')
-        self.name = name
+		Args:
+			name (str): name of the identifier.
+	"""
 
-    def getName(self):
-        """ Return the value of id's name.
+	def __init__(self, name):
+		super().__init__('id')
+		self.name = name
 
-        Returns:
-            (str) name of the id.
-        """
-        return self.name
+	def getName(self):
+		""" Return the value of id's name.
+
+		Returns:
+			(str) name of the id.
+		"""
+		return self.name
 
 
 class numberTree(_AST):
-    """ An AST for a literal number.
-        ATOMIC/LEAF
+	""" An AST for a literal number.
+		ATOMIC/LEAF
 
-        GRAMMAR:
-            num   :-    <int>
-                        <float>
-     
-        Args:
-            value (str): value of the literal number.
-    """
-    def __init__(self, value):
-        super().__init__('literal number')
-        self.value = value
+		GRAMMAR:
+			num   :-    <int>
+						<float>
 
-    def getValue(self):
-        """ Return the value of literal number.
+		Args:
+			value (str): value of the literal number.
+	"""
 
-        Returns:
-            (str) value of the number.
-        """
-        return self.value
+	def __init__(self, value):
+		super().__init__('literal number')
+		self.value = value
+
+	def getValue(self):
+		""" Return the value of literal number.
+
+		Returns:
+			(str) value of the number.
+		"""
+		return self.value
 
 
 class stringTree(_AST):
-    """ An AST for a literal string.
-        ATOMIC/LEAF
+	""" An AST for a literal string.
+		ATOMIC/LEAF
 
-        GRAMMAR:
-            string  :-  string
-     
-        Args:
-            value (str): value of the literal string.
-    """
-    def __init__(self, value):
-        super().__init__('literal string')
-        self.value = value
+		GRAMMAR:
+			string  :-  string
 
-    def getValue(self):
-        """ Return the value of literal string.
+		Args:
+			value (str): value of the literal string.
+	"""
 
-        Returns:
-            (str) value of the string.
-        """
-        return self.value
+	def __init__(self, value):
+		super().__init__('literal string')
+		self.value = value
+
+	def getValue(self):
+		""" Return the value of literal string.
+
+		Returns:
+			(str) value of the string.
+		"""
+		return self.value
 
 
 class assignTree(_AST):
-    """ An AST for a assignment structure.
+	""" An AST for a assignment structure.
 
-        GRAMMAR:
-            assignment  :-  *id assign_op *expr ;
-     
-        Args:
-            assignToken (str): the assignment operator token name.
-    """
-    def __init__(self, assignToken):
-        super().__init__('Assignment')
-        self.assignToken = assignToken
+		GRAMMAR:
+			assignment  :-  *id assign_op *expr ;
 
-    def getToken(self):
-        """ Return the assignment operator token name.
+		Args:
+			assignToken (str): the assignment operator token name.
+	"""
 
-        Returns:
-            (str) the assignment operator token name.
-        """
-        return self.assignToken
+	def __init__(self, assignToken):
+		super().__init__('Assignment')
+		self.assignToken = assignToken
+
+	def getToken(self):
+		""" Return the assignment operator token name.
+
+		Returns:
+			(str) the assignment operator token name.
+		"""
+		return self.assignToken
 
 
 class ifTree(_AST):
-    """ An AST for a ifStatement structure.
+	""" An AST for a ifStatement structure.
 
-        GRAMMAR:
-            ifStatement  :-  if ( *expr ) *block
-                             if ( *expr ) *block else *block
-    """
-    def __init__(self):
-        super().__init__('if statement')
-        
+		GRAMMAR:
+			ifStatement  :-  if ( *expr ) *block
+							 if ( *expr ) *block else *block
+	"""
+
+	def __init__(self):
+		super().__init__('if statement')
+
 
 class whileTree(_AST):
-    """ An AST for a whileStatement structure.
+	""" An AST for a whileStatement structure.
 
-        GRAMMAR:
-            whileStatement  :-  while ( *expr ) *block
-    """
-    def __init__(self):
-        super().__init__('while statement')
+		GRAMMAR:
+			whileStatement  :-  while ( *expr ) *block
+	"""
+
+	def __init__(self):
+		super().__init__('while statement')
 
 
 class returnTree(_AST):
-    """ An AST for a returnStatement structure.
+	""" An AST for a returnStatement structure.
 
-        GRAMMAR:
-            returnStatement  :-  return expr ;
-    """
-    def __init__(self):
-        super().__init__('return statement')
+		GRAMMAR:
+			returnStatement  :-  return expr ;
+	"""
+
+	def __init__(self):
+		super().__init__('return statement')
 
 
 class callTree(_AST):
-    """ An AST for a function call structure.
+	""" An AST for a function call structure.
 
-        GRAMMAR:
-            funcCall  :-  *id ()
-                          *id ( [*expr] )
-    """
-    def __init__(self):
-        super().__init__('function call')
+		GRAMMAR:
+			funcCall  :-  *id ()
+						  *id ( [*expr] )
+	"""
+
+	def __init__(self):
+		super().__init__('function call')
 
 
 class relOPTree(_AST):
-    """ An AST for a relation operation structure.
+	""" An AST for a relation operation structure.
 
-        GRAMMAR:
-            relOp   :-  *expr rel_op *expr
+		GRAMMAR:
+			relOp   :-  *expr rel_op *expr
 
-        Args:
-            relToken (str): the relational operator token name.
-    """
-    def __init__(self, relToken):
-        super().__init__('Relational Operation')
-        self.relToken = relToken
+		Args:
+			relToken (str): the relational operator token name.
+	"""
 
-    def getToken(self):
-        """ Return the relation operator token name.
+	def __init__(self, relToken):
+		super().__init__('Relational Operation')
+		self.relToken = relToken
 
-        Returns:
-            (str) the relation operator token name.
-        """
-        return self.relToken
+	def getToken(self):
+		""" Return the relation operator token name.
+
+		Returns:
+			(str) the relation operator token name.
+		"""
+		return self.relToken
 
 
 class addOPTree(_AST):
-    """ An AST for a addition operation structure.
+	""" An AST for a addition operation structure.
 
-        GRAMMAR:
-            addOp   :-  *expr rel_op *expr
-            
-        Args:
-            addToken (str): the addition operator token name.
-    """
-    def __init__(self, addToken):
-        super().__init__('Additional Operation')
-        self.addToken = addToken
+		GRAMMAR:
+			addOp   :-  *expr rel_op *expr
 
-    def getToken(self):
-        """ Return the addition operator token name.
+		Args:
+			addToken (str): the addition operator token name.
+	"""
 
-        Returns:
-            (str) the addition operator token name.
-        """
-        return self.addToken
+	def __init__(self, addToken):
+		super().__init__('Additional Operation')
+		self.addToken = addToken
+
+	def getToken(self):
+		""" Return the addition operator token name.
+
+		Returns:
+			(str) the addition operator token name.
+		"""
+		return self.addToken
 
 
 class multOPTree(_AST):
-    """ An AST for a multiplication operation structure.
+	""" An AST for a multiplication operation structure.
 
-        GRAMMAR:
-            addOp   :-  *expr rel_op *expr
-                        
-        Args:
-            multToken (str): the multiplication operator token name.
-    """
-    def __init__(self, multToken):
-        super().__init__('Multiplication Operation')
-        self.multToken = multToken
+		GRAMMAR:
+			mulOp   :-  *expr rel_op *expr
 
-    def getToken(self):
-        """ Return the multiplication operator token name.
+		Args:
+			multToken (str): the multiplication operator token name.
+	"""
 
-        Returns:
-            (str) the multiplication operator token name.
-        """
-        return self.multToken
+	def __init__(self, multToken):
+		super().__init__('Multiplication Operation')
+		self.multToken = multToken
+
+	def getToken(self):
+		""" Return the multiplication operator token name.
+
+		Returns:
+			(str) the multiplication operator token name.
+		"""
+		return self.multToken
--- src/jcosim.py
+++ src/jcosim.py
@@ -1,10 +1,13 @@
+import ast
 from os import path
 from getopt import *
 from sys import argv
 from lex import *
 from parse import *
+from ast import *
 from codegen import *
 from symbol_table import *
+from pydot import *
 from pprint import pprint
 
 
@@ -12,125 +15,142 @@ def absPathFromFile(rpath): return path.join(path.dirname(__file__), rpath)
 
 
 def section(title, work):
-    print('-' * 80)
-    print(title)
-    work()
-    print('-' * 80)
+	print('-' * 80)
+	print(title)
+	work()
+	print('-' * 80)
 
 
 def help_text():
-    def work():
-        with open(absPathFromFile('./data/help.txt'), 'r') as f:
-            print(f.read())
-    return "Manual:", work
+	def work():
+		with open(absPathFromFile('./data/help.txt'), 'r') as f:
+			print(f.read())
+
+	return "Manual:", work
 
 
 def token_display(lexer):
-    def work():
-        token_display = '\n'.join(
-            map(str, list(lexer.tokens(ignore=False))))
-        print(token_display)
-        with open('tokens.txt', 'w') as f:
-            f.write(token_display)
-    return "Tokens:", work
+	def work():
+		token_display = '\n'.join(
+			map(str, list(lexer.tokens(ignore=False))))
+		print(token_display)
+		with open('tokens.txt', 'w') as f:
+			f.write(token_display)
+
+	return "Tokens:", work
 
 
 def symtable_display(stb):
-    def work():
-        pprint(stb)
-    return "Symbol Table:", work
+	def work():
+		pprint(stb)
+
+	return "Symbol Table:", work
 
 
-def parsetree_display():
-    def work():
-        with open(absPathFromFile('./data/help.txt'), 'r') as f:
-            print(f.read())
-    return "Manual:", work
+def parsetree_display(program_tree: programTree):
+	def work():
+		graph = Dot(graph_name='Parse Tree', graph_type='graph')
+
+		def start_graph(g, node, parent_node_name=None):
+			g.add_node(Node(name=node.getNodeNum(), shape='plaintext', label=node.getLabel()))
+			if parent_node_name: g.add_edge(Edge(parent_node_name, node.getNodeNum()))
+			if len(node.getKids()) > 0:
+				for kid in node.getKids(): start_graph(g, kid, node.getNodeNum())
+			else:
+				g.add_node(Node(name=f'{node.getNodeNum()}_name', shape='plaintext', label=node.getName()))
+				g.add_edge(Edge(f'{node.getNodeNum()}_name', node.getNodeNum()))
+
+		start_graph(graph, program_tree)
+		graph.write_png('parsetree.png')
+
+	return "Parse Tree:", work
 
 
 def codegen_display():
-    def work():
-        with open(absPathFromFile('./data/help.txt'), 'r') as f:
-            print(f.read())
-    return "Manual:", work
+	def work():
+		with open(absPathFromFile('./data/help.txt'), 'r') as f:
+			print(f.read())
+
+	return "Manual:", work
 
 
 def main():
-    print("JCOSIM: Java Compiler Simulator")
-    try:
-        if len(argv) < 2:
-            raise GetoptError('ERROR: Input file must be specified')
-        options, remainder = getopt(
-            argv[1:],
-            'i:o:stpc:vh',
-            [
-                'input',
-                'output',
-                'symtable',
-                'token',
-                'parsetree',
-                'codegen'
-                'verbose',
-                'help',
-            ])
+	print("JCOSIM: Java Compiler Simulator")
+	try:
+		if len(argv) < 2:
+			raise GetoptError('ERROR: Input file must be specified')
+		options, remainder = getopt(
+			argv[1:],
+			'i:o:stpc:vh',
+			[
+				'input',
+				'output',
+				'symtable',
+				'token',
+				'parsetree',
+				'codegen'
+				'verbose',
+				'help',
+			])
 
-        source = None
-        exe = None
-        symtable = False
-        token = False
-        parsetree = False
-        codegen = False
+		source = None
+		exe = None
+		symtable = False
+		token = False
+		parsetree = False
+		codegen = False
 
-        for opt, arg in options:
-            if opt in ('-h', '--help'):
-                raise GetoptError('')
-            elif opt in ('-i', '--input'):
-                source = arg
-            elif opt in ('-o', '--output'):
-                exe = arg
-            elif opt in ('-s', '--symtable'):
-                symtable = True
-            elif opt in ('-t', '--token'):
-                token = True
-            elif opt in ('-p', '--parsetree'):
-                parsetree = True
-            elif opt in ('-c', '--codegen'):
-                codegen = True
-            elif opt in ('-v', '--verbose'):
-                symtable = True
-                token = True
-                parsetree = True
-                codegen = True
+		for opt, arg in options:
+			if opt in ('-h', '--help'):
+				raise GetoptError('')
+			elif opt in ('-i', '--input'):
+				source = arg
+			elif opt in ('-o', '--output'):
+				exe = arg
+			elif opt in ('-s', '--symtable'):
+				symtable = True
+			elif opt in ('-t', '--token'):
+				token = True
+			elif opt in ('-p', '--parsetree'):
+				parsetree = True
+			elif opt in ('-c', '--codegen'):
+				codegen = True
+			elif opt in ('-v', '--verbose'):
+				symtable = True
+				token = True
+				parsetree = True
+				codegen = True
 
-        if not source:
-            raise GetoptError('ERROR: Input file must be specified')
-        if not exe:
-            exe = path.basename(source)
+		if not source:
+			raise GetoptError('ERROR: Input file must be specified')
+		if not exe:
+			exe = path.basename(source)
 
-        with open(source, 'r') as f:
-            buffer = f.read()
-            lexer = Lexer(buffer)
-            # parser = Parser(lexer)
-            symtable = SymbolTable(lexer)
-            emitter = Emitter("cast1Main")
-            # p = parser.program()  # Start the parser.
-            # code_gen = CodeGen(parser, emitter)
-            # code_gen.generate_code()
-            # print(code_gen.emitter.code)
-            # print("Parsing completed.")
-            if token:
-                section(*token_display(lexer))
-            if symtable:
-                section(*symtable_display(symtable))
-            if parsetree:
-                section(*parsetree_display())
-            if codegen:
-                section(*codegen_display())
+		with open(source, 'r') as f:
+			buffer = f.read()
+			lexer = Lexer(buffer)
+			parser = Parser(lexer)
+			program_tree = parser.program()  # Start the parser.
+			lexer.reset()
+			stb = SymbolTable(lexer)
+			# emitter = Emitter("cast1Main")
+			# code_gen = CodeGen(parser, emitter)
+			# code_gen.generate_code()
+			# print(code_gen.emitter.code)
+			# print("Parsing completed.")
+			if token:
+				section(*token_display(lexer))
+			if symtable:
+				section(*symtable_display(stb))
+			if parsetree:
+				section(*parsetree_display(program_tree))
+			if codegen:
+				section(*codegen_display())
 
-    except GetoptError as e:
-        section(*help_text())
-        print(e)
+	except GetoptError as e:
+		section(*help_text())
+		print(e)
 
 
 if __name__ == '__main__':
-    main()
+	main()
