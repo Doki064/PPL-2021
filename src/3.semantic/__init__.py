261aa8c68439fc2743a17a24927f50c467dcb915
capkimquang <kimquang1920@gmail.com>
2021-05-21 22:05:44 +0700

    [semantic] check variable and function declare twice?

--- src/3.semantic/__init__.py
+++ src/3.semantic/__init__.py
@@ -5,6 +5,8 @@ class Semantic:
     def __init__(self, parser, symbolTable):
         self.ast = parser.program()
         self.symbolTable = symbolTable
+        self.identifier_variable = {}
+        self.identifier_function = {}
 
     def traverse(self, t):
         #################################
@@ -18,7 +20,23 @@ class Semantic:
             if identifier_type is None:
                 raise Exception("Error: Function not found '%s'" % identifier_name)
         #################################
+        #   check function is declared twice?
+        #       declrTree kid:
+        #           *typeTree
+        #           *idTree
+        #           *funcHead
+        #           *block
+        #################################
+        elif isinstance(t, funcDeclTree):
+            identifier_key = t.getKid(2).getKey()
+            identifier_name, identifier_type = self.symbolTable.get_declaration_data(identifier_key)
+            if identifier_name in self.identifier_function:
+                raise Exception("Error: Function '%s' is declared twice." % identifier_name)
+            else:
+                self.identifier_function.add(identifier_name)
+        #################################
         #   check variable is declared?
+        #   check variable is declared twice?
         #       declrTree kid:
         #           *typeTree
         #           *idTree
@@ -28,5 +46,11 @@ class Semantic:
             identifier_name, identifier_type = self.symbolTable.get_declaration_data(identifier_key)
             if identifier_type is None:
                 raise Exception("Error: Variable not found '%s'" % identifier_name)
+            else:
+                if identifier_name in self.identifier_variable:
+                    raise Exception("Error: Variable '%s' is declared twice." % identifier_name)
+                else:
+                    self.identifier_variable.add(identifier_name)
+
 
 
